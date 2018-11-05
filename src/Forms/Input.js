dc1e2837112c0fb270c68c0d342c304bf653a58c
陳詩華 <chenshihua@chenshihuade-MacBook-Air.local>
2018-11-05 14:21:05 +0000

    change input font size

--- src/Forms/Input.js
+++ src/Forms/Input.js
@@ -4,6 +4,7 @@ const Input = styled.input`
     width: 100px;
     padding: 15px 15px;
     border: 2px solid rgba(0,0,0,0.2);
+    font-size: 17px;
     border-radius: 4px 0 0 4px;
     &:focus {
       outline: 0;
